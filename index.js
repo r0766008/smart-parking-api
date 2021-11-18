fd5c9c3abe4402ad42f3433e19246aa7169f98e7
Cody Volz <r0766008@student.thomasmore.be>
2021-11-18 19:38:15 +0100

    Cors update

--- index.js
+++ index.js
@@ -3,19 +3,11 @@ const cors = require("cors");
 
 const app = express();
 
-var corsOptions = {
-  origin: "http://localhost:3000"
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
-});
 
 require("./app/routes/vehicle.routes.js")(app);
 
